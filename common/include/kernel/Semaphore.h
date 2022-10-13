2a1ebee35b8aacda7d0844dbb5a5aa9ee0fbb8ee
hannes hoffmann <hannes.hoffmann@student.tugraz.at>
2022-10-14 00:45:32 +0200

    srater

---
+++ common/include/kernel/Semaphore.h
@@ -0,0 +1,30 @@
+#include "types.h"
+#include <types.h>
+#include "UserThread.h"
+#include "Mutex.h"
+#include "Condition.h"
+#include "umap.h"
+
+class Semaphore
+{
+private:
+    Mutex counter_lock_;
+    Mutex threads_lock_;
+    Mutex condition_lock_;
+    Condition threads_cond_;
+    size_t max_threads_;
+    ustl::map<UserThread*, UserProcess*> waiters_list_;
+    
+public:
+    Semaphore(size_t count) : counter_lock_{"sem::counter_lock_"}, threads_lock_{"sem::threads_lock_"}, condition_lock_{"sem::condition_lock_"}
+     max_threads_{count}, threads_cond{condition_lock_, "sem::condition"}{}
+    ~Semaphore();
+};
+
+Semaphore::Semaphore(/* args */)
+{
+}
+
+Semaphore::~Semaphore()
+{
+}
