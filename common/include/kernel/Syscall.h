401fafaa982bebd353f2e7e5d6559790bab3c07b
Dominik Bek <dbek@student.tugraz.at>
2022-10-30 19:30:50 +0100

    clock test 1

--- common/include/kernel/Syscall.h
+++ common/include/kernel/Syscall.h
@@ -26,8 +26,8 @@ class Syscall
   static size_t wait_pid(size_t arg1, size_t* arg2, size_t arg3);
   static int get_pid();
   static unsigned int sleep(unsigned int seconds);
-  static size_t clock();
-
+  static unsigned int clock();
+  static size_t getRDTSC();
   // pthreads
   static size_t pthread_create(size_t thread, size_t attr, size_t start_routine, size_t arg, size_t wrapper);
   static void pthread_exit(void* value);
--- common/source/kernel/Syscall.cpp
+++ common/source/kernel/Syscall.cpp
@@ -7,6 +7,7 @@
 #include "UserProcess.h"
 #include "ProcessRegistry.h"
 #include "File.h"
+#include "../../../userspace/libc/include/time.h"
 
 
 #define callingThread (UserThread*) currentThread 
@@ -518,14 +519,28 @@ unsigned int Syscall::sleep(unsigned int seconds)
         y = j + i;
   return y;
 }
+// The clock() function returns an approximation of processor time used by the program
+unsigned int Syscall::clock()
+{
+  size_t firstbits;
+  size_t lastbits; 
+  // The processor monotonically increments the time-stamp counter MSR
+  // every clock cycle and resets it to 0 whenever the processor is reset.
+  asm volatile("rdtsc \n\t" : "=a"(lastbits), "=d"(firstbits));
+  size_t return_ = firstbits << 32 | lastbits;
+  //uint32 new_ticks = Scheduler::instance()->getTicks(); 
+  //size_t reuturn_d_ticks = return_ / new_ticks;
+  //size_t return_final = reuturn_d_ticks/1000000;
+  return ((return_/54) / CLOCKS_PER_SEC); //CLOCKS_PER_SECOND * (RUNNING TIME OF WHATEVER)
+}
 
-size_t Syscall::clock()
+size_t Syscall::getRDTSC()
 {
   size_t firstbits;
   size_t lastbits; 
   asm volatile("rdtsc \n\t" : "=a"(lastbits), "=d"(firstbits));
   //debug(USERPROCESS,"read %ld from tsc and MAX STACKS btw is %lld offset is %ld!!\n", rand, MAX_STACKS, page_offset);
-  size_t return_ = firstbits << 32 | lastbits;
+  size_t return_ = lastbits << 32 | firstbits;
   return return_;
 }
 
