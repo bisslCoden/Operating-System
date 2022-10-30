d6d322de4d9c4490b0adfc244d8833f0901b8f59
Thomas Mikl <thomas.mikl@student.tugraz.at>
2022-10-30 12:03:58 +0100

    oh i am once again an idiot. exec without args was already working but my test wasn't doing anything without args passed.

--- userspace/tests/printer.c
+++ userspace/tests/printer.c
@@ -4,7 +4,16 @@
 
 int main(int argc, const char *argv[])
 {
+  printf("|----------------------------------------\n"
+         "|---your beatutifully passed arguments---\n"
+         "|----------------------------------------\n"
+         "| \n");
+
   for (int i = 0; i < argc; i++) 
-    printf("argv[%d] = '%s'\n", i, argv[i]);
+    printf("| - argv[%d] = '%s'\n|  ", i, argv[i]);
+    
+  printf("|----------------------------------------\n"
+         "|----finished printing your arguments----\n"
+         "|----------------------------------------\n");
   return 0;
 }
