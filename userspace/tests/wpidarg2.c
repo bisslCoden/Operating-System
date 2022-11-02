4ca3eff35c13f90e98ab29b779a5e88a4bb4cb8a
Dominik Bek <dbek@student.tugraz.at>
2022-11-02 20:46:51 +0100

    for check out

--- userspace/tests/wpidarg2.c
+++ userspace/tests/wpidarg2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
 #include <wait.h>
@@ -13,26 +14,20 @@ int main(int argc, char** argv)
     printf("Call to wait_pid!\n");
     int pid = fork();
     pid = fork();
-    pid = fork();
-    
     printf("pid : %d, and mine process id: %d\n", pid, getpid());
     if(pid != 0)
     {
       printf("pid before waitpid: %d\n", pid);
-      int p_id = waitpid(pid, 0, 0);
-      printf("pid before if: %d\n", pid);
+      int p_id = waitpid(-1, NULL, NULL);
       if(p_id < 0 || pid < 0)
       {
         printf("some error happend\n");
         return -1;
       }
       else
-      {
        printf("Waited for child with pid: %d\n", p_id);    
-      }
     }
     printf("Finished the task, my pid: %d\n", getpid());
   }
-  
   return 0;
 }
