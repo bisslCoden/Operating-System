2bd386a7a6da3ed478bbc4a9ffb06afc38e053e3
Dominik Bek <dbek@student.tugraz.at>
2022-11-08 18:46:42 +0100

    with sleep

--- common/source/kernel/Syscall.cpp
+++ common/source/kernel/Syscall.cpp
@@ -599,7 +599,7 @@ int Syscall::get_pid()
   // frequency is needed, with that we multiply clock_per_sec
 unsigned int Syscall::sleep(unsigned int seconds)
 {
-  /*debug(SLEEP, "Sleep system call started\n");
+  debug(SLEEP, "Sleep system call started\n");
   uint64_t rdtsc_now = Scheduler::instance()->getRDTSC() * 10;
   debug(SLEEP, "rdtsc_now:    %ld\n", rdtsc_now);
   uint64_t time_to_wake = (seconds * 185) * Scheduler::instance()->getRDTSCdiff() + rdtsc_now;
@@ -615,9 +615,13 @@ unsigned int Syscall::sleep(unsigned int seconds)
      //Scheduler::instance()->getRDTSC()/(CLOCKS_PER_SEC * 20 ),
      //Scheduler::instance()->getFrequency());
     Scheduler::instance()->yield();
-  }*/
+  }
   return 0;
 }
+
+// 71.6 % 
+//
+
 
 // rdtsc now - rdtsc at program start
 // but thread can sleep or yield, so then it doesn't count
