e33e38818902fc36799d796b30b34e8db00864ef
Dominik Bek <dbek@student.tugraz.at>
2022-11-09 11:58:01 +0100

    float test added old with comma deleted

--- userspace/tests/sleepfloat.c
+++ userspace/tests/sleepfloat.c
@@ -9,7 +9,7 @@
 int main(int argc, char** argv)
 {
   printf("Sleep test started!\n"); // clock return RDTSC in this case
-  int x = sleep(5)(m);
+  int x = sleep(5.3);
   printf("Sleep test enden!\n"); // clock return RDTSC in this case
   return x;
 }
