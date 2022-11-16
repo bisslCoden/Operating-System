b130e7021974bd5a47d8857216706f73469728d6
Dominik Bek <dbek@student.tugraz.at>
2022-11-16 23:48:31 +0100

    clock try 20?

--- common/source/kernel/UserThread.cpp
+++ common/source/kernel/UserThread.cpp
@@ -72,6 +72,10 @@ bool UserThread::schedulable(){
           if (getflags()->kcancelreq.test_and_set())
           {
             setLastStart(Scheduler::instance()->getRDTSC());
+            if(was_scheduled_ == 0) // variables used in scheduler need to be atomic
+            {
+               setLastStart(Scheduler::instance()->getRDTSC());
+            }
             was_scheduled_ = 1;
             return true;
           }
@@ -112,13 +116,21 @@ bool UserThread::schedulable(){
     }
     else if (sleepy == AWAKE_KS)
     {
-      setLastStart(Scheduler::instance()->getRDTSC());
+      if(was_scheduled_ == 0) // variables used in scheduler need to be atomic
+      {
+         setLastStart(Scheduler::instance()->getRDTSC());
+      }
       // my_pages_lock_.release();
       was_scheduled_ = 1;
       return true;
     }
     else if(DYING_)
     {
+      if(was_scheduled_ == 0) // variables used in scheduler need to be atomic
+      {
+         setLastStart(Scheduler::instance()->getRDTSC());
+      }
+      was_scheduled_ = 1;
       return true;
     }
     else
