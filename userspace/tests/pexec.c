4e8f5a2b250b09b6e67966847fe50d2fe7789dca
Thomas Mikl <thomas.mikl@student.tugraz.at>
2022-11-18 09:57:05 +0100

    this test should not call pcreate.sweb

--- userspace/tests/pexec.c
+++ userspace/tests/pexec.c
@@ -11,21 +11,23 @@ pthread_t tids[PTHREADS];
 char* const path1 = "/usr/pcreate.sweb";
 
 void forkThenExec(char* const args[]){
+  printf("forking\n");
   pid_t pid = fork();
-  if (pid == 0)
-    assert(execv(args[0], args) < 1);
+  int ret = execv(args[0], args);
+  printf("exec returned.. ret = %d\n", ret);
 }
 
 int main()
 {
-  printf("[main] starting pfork now...\n");
+  printf("[main] starting pexec now...\n");
+  pthread_t ret;
 
   for(size_t i = 0; i < PTHREADS; ++i)
   {
     char* const args_cur[] = {path1, "one", "two", "thee", "quack", NULL};
-    assert(pthread_create(&tids[i], NULL, (void*(*)(void *))&forkThenExec, (void*)args_cur) == 0 && "couldnt create anymore threads daaamn!\n");
+    ret = pthread_create(&tids[i], NULL, (void*(*)(void *))&forkThenExec, (void*)args_cur) == 0 && "couldnt create anymore threads daaamn!\n";
   }
 
-  printf("[main]: successfully exiting the programm!\n");
+  printf("[main]: successfully exiting the programm! pcreate ret = %ld\n", ret);
   return 0;
 }
