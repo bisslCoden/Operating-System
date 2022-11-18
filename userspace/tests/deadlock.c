87c2ec415951bcc393aed63ec33f1d8a268a6323
Jan Maric <jan.maric@student.tugraz.at>
2022-11-18 10:55:51 +0100

    rewrote deadlock test

--- userspace/tests/deadlock.c
+++ userspace/tests/deadlock.c
@@ -1,34 +1,32 @@
-#include <stdio.h>
-#include <pthread.h>
+#include "stdio.h"
+#include "pthread.h"
+#include "stdlib.h"
+#include "assert.h"
 
-size_t p;
+pthread_t tid_1, tid_2;
 
-void* secondThread() {
-  size_t ret_value;
-  printf("WAITING - THREAD 2\n");
-  ret_value = pthread_join(p, NULL);
-  printf("%ld\n", ret_value);
-  return NULL;
+// deadlock creation
+void *thread1_deadlock_thread2() {
+  sleep(2);
+  int join1 = pthread_join(tid_2, NULL);
+  printf("Join of thread1 trying to deadlock itself returns = %d\n", join1);
+
+  return (void *) 111;
 }
 
+void *thread2_deadlock_thread1() {
+  sleep(2);
+  int join2 = pthread_join(tid_1, NULL);
+  printf("Join of thread2 trying to deadlock itself returns = %d\n", join2);
 
-void* firstThread() {
-  size_t ret_value, tid;
-  printf("WAITING - THREAD 1\n");
-  sleep(10);
-
-  pthread_create(&tid, NULL, secondThread, NULL);
-  ret_value = pthread_join(tid, NULL);
-  printf("%ld\n", ret_value);
-  return NULL;
+  return (void *) 111;
 }
 
+int main(int argc, char *argv[]) {
+  int create1_retval = pthread_create(&tid_1, NULL, thread1_deadlock_thread2, NULL);
+  int create2_retval = pthread_create(&tid_2, NULL, thread2_deadlock_thread1, NULL);
 
-int main() {
-  p = pthread_self();
-  size_t tid;
-  pthread_create(&tid, NULL, firstThread, NULL);
-  pthread_join(tid, NULL);
-
+  assert((create1_retval == 0 && create2_retval == 0) && "Thread1 didnt get created successfully!\n"); ///
+  getchar();
   return 0;
 }
